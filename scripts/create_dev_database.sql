c08be00389a40fa376deaa04f2749678d5e92eca
Pupsik2001 <kenguru13422431@gmail.com>
2023-12-23 21:13:07 +0600

    Push template

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER "pizza" SUPERUSER;
+CREATE DATABASE "pizza" OWNER "pizza" ENCODING 'utf-8';
